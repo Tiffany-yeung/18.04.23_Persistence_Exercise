177f94d786c7be6fc31c9689cd04d5c69786de4d
Tiffany Yeung <tiffany.yeung@qa.com>
2018-04-23 12:20:08 +0100

    First commit

---
+++ src/main/java/com/qa/accounts/Account.java
@@ -0,0 +1,32 @@
+package com.qa.accounts;
+
+public class Account {
+    	
+	private String firstName;
+	private String lastName;
+	private int accountNo;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public int getAccountNo() {
+		return accountNo;
+	}
+
+	public Account (String firstName, String lastName, int accountNo) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.accountNo = accountNo;
+	}
+	
+	public String toString() {
+		return "First Name: " + firstName + ". Last Name: " + lastName 
+				+ ". Account Number: " + accountNo;
+	}
+	
+}
---
+++ src/main/java/com/qa/accounts/Service.java
@@ -0,0 +1,34 @@
+package com.qa.accounts;
+
+import java.util.HashMap;
+
+public class Service {
+	
+		static int keyNumber = 1;
+        
+		private HashMap<Integer, Account> bankData = new HashMap<Integer, Account>();
+		
+		public HashMap<Integer, Account> getBankData() {
+			return bankData;
+		}
+
+		public void setBankData(HashMap<Integer, Account> bankData) {
+			this.bankData = bankData;
+		}
+
+		public void addAccount(Account account) {
+			
+			if(bankData.containsKey(account.getAccountNo())) {
+				System.out.println("Account already exists.");
+			}
+			else {
+				bankData.put(keyNumber, account);
+				keyNumber++;
+			}
+		}
+		
+		public Account getAccount(int keyNumber) {
+			System.out.println(bankData.get(keyNumber));
+			return bankData.get(keyNumber);
+		}
+}
